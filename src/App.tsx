56b5732b66d1dfaa9cf8db5eb4d59bcfdddd7fcd
kevinshi97 <kevinsshi@hotmail.com>
2021-05-05 03:04:22 -0400

    done for the day

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hexagon from './components/Hexagon/Hexagon';
 
 function App() {
+  const onMouseEnter = (e: MouseEvent) => {
+    console.log('onMouseEnter App');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +23,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Hexagon
+        onMouseEnter={e => onMouseEnter(e)}
+      />
     </div>
   );
 }
---
+++ src/components/HexagonGrid/HexagonGrid.tsx
@@ -0,0 +1,7 @@
+function HexagonGrid() {
+  return (
+    <div>Hexagon Grid</div>
+  )
+}
+
+export default HexagonGrid;
